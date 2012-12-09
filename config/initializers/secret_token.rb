c30296c73b429454883d180d0bc7f6b67a012cf6
Brad Robertson <bradleyrobertson@gmail.com>
2012-12-09 14:48:52 -0500

    skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseOmniauthExample::Application.config.secret_token = '4c39664183d23345aaa93a5f519e7e68d7ce5800f60c1793f6b9f4c7566cff88a77aa6cff0303379e442dd35c21e78571547fce560febd743d43aba6e53a6138'
